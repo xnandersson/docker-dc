cb79a391ddd458025121b5a8e2d8a1adfcfd1da7
Niklas Andersson <niklas.andersson@openforce.se>
2019-03-26 18:14:27 +0100

    Major refactoring

---
+++ src/dcpromo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+SAMBA_DOMAIN = os.getenv('SAMBA_DOMAIN', 'acme')
+SAMBA_HOST_NAME = os.getenv('SAMBA_HOST_NAME', 'dc')
+SAMBA_ADMINPASS = os.getenv('SAMBA_ADMINPASS', 'Abc123')
+SAMBA_KRBTGTPASS = os.getenv('SAMBA_KRBTGTPASS', SAMBA_ADMINPASS)
+SAMBA_REALM = os.getenv('SAMBA_REALM', 'ACME.ORG')
+
+def promote(samba_domain=None, samba_host_name=None, samba_adminpass=None, samba_krbtgtpass=None, samba_realm=None): 
+    proc = subprocess.Popen([
+        'samba-tool', 'domain', 'provision', 
+        '--domain', samba_domain, 
+        '--host-name', samba_host_name,
+        '--adminpass', samba_adminpass,
+        '--krbtgtpass', samba_krbtgtpass,
+        '--realm', samba_realm])
+    proc.wait()
+    proc = subprocess.Popen(['cp', '/var/lib/samba/private/krb5.conf', '/etc/krb5.conf'])
+    proc.wait()
+    proc = subprocess.Popen(['samba-tool', 'user', 'create', 'nandersson', 'Secret012'])
+    proc.wait()
+
+def service_start(service):
+    proc = subprocess.Popen(['service', service, 'start'])
+    rc = proc.wait()
+    return True if rc == 0 else False
+
+def sshd():
+    proc = subprocess.Popen(['/usr/sbin/sshd', '-D'])
+    proc.wait()
+
+if __name__ == '__main__':
+    if not os.path.isfile('/etc/krb5.conf'):
+        promote(samba_domain=SAMBA_DOMAIN, samba_host_name=SAMBA_HOST_NAME, samba_adminpass=SAMBA_ADMINPASS, samba_krbtgtpass=SAMBA_KRBTGTPASS, samba_realm=SAMBA_REALM)
+    service_start('samba-ad-dc')
+    sshd()
--- src/docker-dc.py
+++ src/docker-dc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import docker
 import os
+import shutil
 import uuid
 
 BUILD_DIR = '/tmp/{uuid}/'.format(uuid=uuid.uuid4().hex)
@@ -11,62 +12,14 @@ def mkdir_build_dir():
   except FileExistsError as e:
     pass
 
-def create_dcpromo_debconf():
-  with open(os.path.join(BUILD_DIR, 'dcpromo'), 'w') as f:
-    f.write("""#!/bin/bash
-if [ ! -f /etc/krb5.conf ]; then
-samba-tool domain provision \
-  --domain=${SAMBA_DOMAIN} \
-  --host-name=${SAMBA_HOST_NAME} \
-  --adminpass=${SAMBA_ADMINPASS} \
-  --krbtgtpass=${SAMBA_KRBTGTPASS} \
-  --realm=${SAMBA_REALM}
-cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
-samba-tool user create nandersson Secret012
-fi
-service samba-ad-dc start
-/usr/sbin/sshd -D\n""")
-
-def create_dcpromo_template():
-  with open(os.path.join(BUILD_DIR, 'dcpromo.template'), 'w') as f:
-    f.write("""#!/bin/bash
-samba-tool domain provision \
-  --domain=openforce \
-  --host-name=DC \
-  --adminpass=Abc123!! \
-  --krbtgtpass=Abc123!! \
-  --machinepass=Abc123!! \
-  --dnspass=Abc123!! \
-  --root=root \
-  --nobody=nobody \
-  --users=staff \
-  --server-role=dc \
-  --use-rfc2307 \
-  --realm=OPENFORCE.ORG \
-  --dns-backend=SAMBA_INTERNAL\n""")
-
-def create_dockerfile():
-  with open(os.path.join(BUILD_DIR, 'Dockerfile'), 'w') as f:
-    f.write("""FROM ubuntu:bionic
-MAINTAINER Niklas Andersson <niklas.andersson@openforce.se>
-ENV UPDATED_ON 2018-10-02
-RUN apt-get update -y
-RUN apt-get install samba -y
-RUN apt-get install rsyslog nmap smbclient -y
-RUN apt-get install ssh -y
-RUN rm /etc/samba/smb.conf
-RUN apt-get install vim -y
-ADD dcpromo.template /usr/local/bin/
-ADD dcpromo /usr/local/bin/dcpromo
-RUN chmod +x /usr/local/bin/dcpromo
-RUN mkdir /run/sshd
-EXPOSE 22 53 88 135 139 389 445 464 636 1024 3268 3269
-CMD /bin/bash\n""")
+def copy_files_to_build_dir():
+    shutil.copyfile('Dockerfile', os.path.join(BUILD_DIR, 'Dockerfile')
+    shutil.copyfile('dcpromo.py', os.path.join(BUILD_DIR, 'dcpromo.py')
+    proc = subprocess.Popen(['chmod', '+x', os.path.join(BUILD_DIR, 'dcpromo.py'])
+    proc.wait()
 
 if __name__ == '__main__':
   mkdir_build_dir()
-  create_dcpromo_debconf()
-  create_dcpromo_template()
-  create_dockerfile()
+  copy_files_to_build_dir()
   client = docker.DockerClient(base_url='unix://var/run/docker.sock')
   client.images.build(path=BUILD_DIR, tag='xnandersson/samba-ad-dc', rm=True, pull=True)
